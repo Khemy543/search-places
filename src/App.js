3ac5cf2593ba0912f18c010d8f415b02c874518f
Gideon Assafuah <gassafuah@gmail.com>
2022-04-03 12:10:59 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [searhKey, setSearchKey] = React.useState("");
+  const [places, setPlaces] = React.useState([]);
+
+  React.useEffect(() => {
+    const MAP_BOX_TOKEN =
+      "pk.eyJ1IjoiZ2Vibzk2IiwiYSI6ImNsMWY0NzczZzByZnQzcXFpanBpczBjcXQifQ.0w75NzO1o2vxGyuauC-vRA";
+    async function fetchData() {
+      const response = await axios.get(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${searhKey}.json?access_token=${MAP_BOX_TOKEN}`
+      );
+
+      if (response) {
+        const placeNames = response.data?.features;
+        setPlaces(placeNames);
+      }
+    }
+    fetchData();
+  }, [searhKey]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout">
+      <form>
+        <div>
+          <input
+            name="search"
+            type="search"
+            value={searhKey}
+            onChange={(e) => setSearchKey(e.target.value)}
+            className="search-input"
+            placeholder="Search place... "
+          />
+        </div>
+      </form>
+
+      <div>
+        {searhKey && <h5>Search Results for {searhKey}</h5>}
+        <ul>
+          {places.map((place) => (
+            <li key={place.id}>{place.place_name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
